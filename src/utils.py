506fe6d9d190a9284b077c60570a3630df439112
WildBunnie <wildbunnie2003@gmail.com>
2024-11-08 10:42:20 +0000

    now the classes to be checked are chosen in the predict function instead of using the coco.txt file

--- src/utils.py
+++ src/utils.py
@@ -5,9 +5,6 @@ from ultralytics import YOLO
 
 model = YOLO('yolo11s.pt')
 
-with open("coco.txt", "r") as coco_file:
-    things = coco_file.read().split("\n")
-
 def is_rtsp_link_working(rtsp_url: str) -> bool:
 
     try:
@@ -50,7 +47,8 @@ def get_parking_info(rstp_url, parking_spots=None):
             continue
 
         # Predict with YOLO
-        results = model.predict(frame)
+        # 2 - car | 5 - bus | 7 - truck
+        results = model.predict(frame, classes=[2, 5, 7])
         a = results[0].boxes.data
         px = pd.DataFrame(a).astype("float")
 
@@ -68,22 +66,19 @@ def get_parking_info(rstp_url, parking_spots=None):
             y1 = int(row[1])
             x2 = int(row[2])
             y2 = int(row[3])
-            thing_id = int(row[5])
-            thing = things[thing_id]
             
-            if thing in ["truck", "car", "bus"]:
-                cx = int((x1 + x2) // 2)
-                cy = int((int((y1 + y2) // 2) + y2) // 2)
-                cv2.circle(frame, (cx, cy), 3, (0, 0, 255), -1)
+            cx = int((x1 + x2) // 2)
+            cy = int((int((y1 + y2) // 2) + y2) // 2)
+            cv2.circle(frame, (cx, cy), 3, (0, 0, 255), -1)
 
-                if parking_spots:
-                    for spot in parking_spots:
-                        pts = np.array(spot['points'], np.int32)
-                        if cv2.pointPolygonTest(pts, (cx, cy), False) >= 0:
-                            cv2.polylines(frame, [pts], True, (0, 0, 255), 2)
-                            if total_spots != 0:
-                                occupied_spots += 1
-                            break
+            if parking_spots:
+                for spot in parking_spots:
+                    pts = np.array(spot['points'], np.int32)
+                    if cv2.pointPolygonTest(pts, (cx, cy), False) >= 0:
+                        cv2.polylines(frame, [pts], True, (0, 0, 255), 2)
+                        if total_spots != 0:
+                            occupied_spots += 1
+                        break
         cap.release()
 
         return frame, occupied_spots, total_spots
