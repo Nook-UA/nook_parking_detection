cf30c5e422128abb5510d464b93c9033b79e1ae5
João Andrade <31666693+WildBunnie@users.noreply.github.com>
2024-11-10 18:14:16 +0000

    [Esnook 59] Parking Detection Microservice (#1)
    * initial commit. no parking detection yet, only car detection
    
    * changed file structure to a proper fast api file structure and added support for adding parking spot editing and detection
    
    * added a docker to run the program
    
    * removed the video streaming from the docker and some minor changes to the dockerfile
    
    * changed back the format to something simpler
    
    * added function that checks wheter a rtsp link is valid/working
    
    * added tests for the endpoints
    
    * added missing extention to the image path
    
    * added some resilience in case the RTSP url stops working
    
    * now the classes to be checked are chosen in the predict function instead of using the coco.txt file
    
    * model now also looks for motorcycles
    
    * redis host url is now fetched from an enviroment variable instead of being hardcoded

---
+++ src/main.py
@@ -0,0 +1,101 @@
+from fastapi import FastAPI, BackgroundTasks, HTTPException
+from fastapi.responses import FileResponse
+
+from src.schemas import ParkingLot, ParkingSpot
+from src.utils import get_parking_info, is_rtsp_link_working
+
+import redis
+import os
+import cv2
+import json
+import time
+
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = int(os.getenv('REDIS_PORT', 6379))
+REDIS_DB = int(os.getenv('REDIS_DB', 0))
+
+db = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_DB)
+
+IMAGE_DIR = "./images"
+if not os.path.exists(IMAGE_DIR):
+    os.makedirs(IMAGE_DIR)
+
+DELAY_BETWEEN_CHECKS = 10
+
+app = FastAPI()
+
+def start_parking_lot_service(parking_lot: ParkingLot):
+    while True:
+
+        parking_spots = db.get(f"parking_lot:{parking_lot.id}:parking_spots")
+        if parking_spots:
+            parking_spots = json.loads(parking_spots)
+        else:
+            parking_spots = None
+
+        frame, occupied_spots, total_spots = get_parking_info(parking_lot.rstp_url, parking_spots)
+        if frame is not None:
+            image_path = os.path.join(IMAGE_DIR, f"{parking_lot.id}.png")
+            cv2.imwrite(image_path, frame)
+
+            occupancy_data = {
+                "freed": total_spots - occupied_spots,
+                "occupied": occupied_spots,
+                "total": total_spots
+            }
+        else:
+            occupancy_data = {
+                "ERROR": "Cannot acess the RTSP URL"
+            }
+            
+        db.set(f"parking_lot:{parking_lot.id}:occupancy", json.dumps(occupancy_data))
+        time.sleep(DELAY_BETWEEN_CHECKS)
+
+@app.post("/add_parking_lot/", status_code=201)
+async def add_parking_lot(parking_lot: ParkingLot, background_tasks: BackgroundTasks):
+
+    if db.get(f"parking_lot:{parking_lot.id}"):
+        raise HTTPException(status_code=409, detail="Parking lot already exists")
+
+    if not is_rtsp_link_working(parking_lot.rstp_url):
+        raise HTTPException(status_code=400, detail="Failed to capture frame from RTSP stream")
+
+    db.set(f"parking_lot:{parking_lot.id}", parking_lot.rstp_url)
+
+    background_tasks.add_task(start_parking_lot_service, parking_lot)
+
+    return {"status": f"Parking lot '{parking_lot.id}' added"}
+
+@app.get("/parking_lot/{parking_lot_id}", status_code=200)
+async def get_parking_lot_info(parking_lot_id: str):
+    
+    if not db.get(f"parking_lot:{parking_lot_id}"):
+        raise HTTPException(status_code=404, detail="Parking lot not found")
+
+    occupancy = json.loads(db.get(f"parking_lot:{parking_lot_id}:occupancy"))
+
+    return {
+        "parking_lot_id": parking_lot_id,
+        "image_url": f"/images/{parking_lot_id}.png",
+        "occupancy": occupancy
+    }
+
+@app.post("/parking_lot/{parking_lot_id}/spots")
+async def set_parking_spots(parking_lot_id: str, spots: list[ParkingSpot]):
+
+    if not db.get(f"parking_lot:{parking_lot_id}"):
+        raise HTTPException(status_code=404, detail="Parking lot not found")
+    
+    # convert the ParkingSpots to dicts to be able to store them in redis
+    spots = [spot.model_dump() for spot in spots]
+    db.set(f"parking_lot:{parking_lot_id}:parking_spots", json.dumps(spots))
+
+    return {"status": f"Parking spots added to '{parking_lot_id}'"}
+
+@app.get("/images/{image_name}")
+async def serve_image(image_name: str):
+    image_path = os.path.join(IMAGE_DIR, image_name)
+    if not os.path.exists(image_path):
+        raise HTTPException(status_code=404, detail="Image not found")
+
+    return FileResponse(image_path, media_type="image/png")
